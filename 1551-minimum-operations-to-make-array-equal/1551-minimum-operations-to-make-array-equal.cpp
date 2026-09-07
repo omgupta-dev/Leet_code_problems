afea6ec233e1c7c8f73dfdd5e50cb4ccde8ef09c
Om Gupta <omgupta.react@gmail.com>
2026-09-07 18:56:43 +0530

    Time: 0 ms (100%), Space: 8 MB (77.19%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-        vector<int>arr(n);
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=(2*i)+1;
-        }
-        int op=0;
-        int l=0,r=n-1;
-        while(l<r)
-        {
-            op+=(arr[r]-arr[l])/2;
-            l++;
-            r--;
-        }
-        return op;
+        return (n*n)/4;
     }
 };
