1cc6b927655888ac89f36f54cbf29230ffdd1351
Om Gupta <omgupta.react@gmail.com>
2026-09-07 18:17:49 +0530

    Time: 3 ms (17.01%), Space: 9.4 MB (82.39%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        map<char,int>m;
+        for(int i=0;i<s.size();i++)
+        {
+            if(!m.contains(s[i]))
+                m[s[i]]=1;
+            else
+                m[s[i]]++;
+        }
+        map<char,int> :: iterator it=m.begin();
+        int k=it->second;
+        while(it!=m.end())
+        {
+            if(it->second!=k) return false;
+            it++;
+        }
+        return true;
+    }
+};
