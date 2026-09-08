d4bbf4adb6cd2a8a6621f107af2c571b76a1827d
Om Gupta <omgupta.react@gmail.com>
2026-09-09 00:12:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (39.35%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int ans=0;
+        unordered_map<char,int>m;
+        for(int i=0;i<jewels.size();i++)
+        {
+            m[jewels[i]]=1;
+        }
         for(int i=0;i<stones.size();i++)
         {
-            for(int j=0;j<jewels.size();j++)
+            if(m.contains(stones[i]))
             {
-                if(stones[i]==jewels[j])
-                {
-                    ans++;
-                }
+                ans++;
             }
         }
         return ans;
