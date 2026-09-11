3543a6a6c70e785df1181cbbbaf12a17d95c5673
Om Gupta <omgupta.react@gmail.com>
2026-09-12 00:44:50 +0530

    Time: 2 ms (54.19%), Space: 7.8 MB (39.4%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int ans=a|b;
-        int count=0;
-        for(int i=0;i<32;i++)
-        {
-            int bit=(c>>i)&1;
-            if(bit)
-            {
-                if(((ans>>i)&1)==0)
-                    count++;
-            }
-            else
-            {
-                if((a>>i)&1)
-                {
-                    count++;
-                }
-                if((b>>i)&1)
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
+        return __builtin_popcount((a|b)^c)+__builtin_popcount(a&b&((a|b)^c));
     }
 };
