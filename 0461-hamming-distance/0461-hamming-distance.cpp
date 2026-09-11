4c18d22b090b4741e3975476e613b0840d1f7f03
Om Gupta <omgupta.react@gmail.com>
2026-09-11 10:36:59 +0530

    Time: 2 ms (1.51%), Space: 8 MB (35.16%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z=x^y;
+        int ans=0;
+        while(z)
+        {
+            int bit=z%2;
+            z/=2;
+            if(bit)
+                ans++;
+        }
+        return ans;
+    }
+};
