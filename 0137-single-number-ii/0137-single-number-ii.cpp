b5c90638496c74d5e5f0964097b9bce421a5826d
Om Gupta <omgupta.react@gmail.com>
2026-09-12 22:57:04 +0530

    Time: 0 ms (100%), Space: 13.5 MB (35.67%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i+=3)
+        int ones=0,twos=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]!=nums[i-1])
-                return nums[i-1];
+            ones=(ones^nums[i])&(~twos);
+            twos=(twos^nums[i])&(~ones);
         }
-        return nums[nums.size()-1];
+        return ones;
     }
 };
