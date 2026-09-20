f1930e1e20ffc01f1e355b3def036d04688da229
Om Gupta <omgupta.react@gmail.com>
2026-09-20 13:10:10 +0530

    Time: 3 ms (16.14%), Space: 9.9 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int rev=s[i]-'z'-1;
+            ans+=abs(rev)*(i+1);
+        }
+        return ans;
+    }
+};
