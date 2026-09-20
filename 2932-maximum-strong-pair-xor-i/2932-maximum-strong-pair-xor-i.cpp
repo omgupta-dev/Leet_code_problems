e11f3dde03b2ab43d8146a87236aa67e9b787079
Om Gupta <omgupta.react@gmail.com>
2026-09-20 14:01:05 +0530

    Time: 0 ms (100%), Space: 27.9 MB (35.8%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
-        vector<int>x;
-        vector<int>y;
+        int mxor=0;
         for(int i=0;i<nums.size();i++)
         {
             for(int j=i;j<nums.size();j++)
             {
-                if(abs(nums[i]-nums[j])<=min(nums[i],nums[j]))
+                int x=(nums[i]^nums[j]);
+                if(mxor<x && abs(nums[i]-nums[j])<=min(nums[i],nums[j]))
                 {
-                    x.push_back(nums[i]);
-                    y.push_back(nums[j]);
+                    mxor=x;
                 }
             }
-        }
-        if(x.empty()) return 0;
-        int mxor=x[0]^y[0];
-        for(int i=0;i<x.size();i++)
-        {
-            if(mxor<(x[i]^y[i]))
-                mxor=x[i]^y[i];
         }
         return mxor;
     }
