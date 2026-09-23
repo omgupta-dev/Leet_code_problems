bd06c5e0b6a504de9bb8d3f958009795d6faa429
Om Gupta <omgupta.react@gmail.com>
2026-09-23 10:13:01 +0530

    Time: 0 ms (100%), Space: 11.4 MB (61.03%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>expected=heights;
+        sort(expected.begin(),expected.end());
+        int ans=0;
+        for(int i=0;i<expected.size();i++)
+        {
+            if(expected[i]!=heights[i])
+                ans++;
+        }
+        return ans;
+    }
+};
