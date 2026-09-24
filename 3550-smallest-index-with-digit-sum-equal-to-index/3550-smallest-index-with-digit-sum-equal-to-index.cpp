6f00565764ad462de471cd598825144e3457232e
Om Gupta <omgupta.react@gmail.com>
2026-09-24 11:42:43 +0530

    Time: 0 ms (100%), Space: 31.1 MB (4.04%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            int temp=nums[i];
+            int sum=0;
+            while(temp)
+            {
+                sum+=temp%10;
+                temp/=10;
+            }
+            if(i==sum)
+                return i;
+        }
+        return -1;
+    }
+};
