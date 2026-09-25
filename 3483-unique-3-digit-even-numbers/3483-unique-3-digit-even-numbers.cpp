87ac68072be5dc2778be12d65599267b351989fa
Om Gupta <omgupta.react@gmail.com>
2026-09-25 10:40:29 +0530

    Time: 5 ms (50.79%), Space: 30.9 MB (45.5%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        unordered_set<int>s;
+        for(int i=0;i<digits.size();i++)
+        {
+            for(int j=0;j<digits.size();j++)
+            {
+                for(int k=0;k<digits.size();k++)
+                {
+                    if(i!=j && j!=k && k!=i)
+                    {
+                        if(digits[i]!=0 && digits[k]%2==0)
+                        {
+                            s.insert(digits[i]*100+digits[j]*10+digits[k]);
+                        }
+                    }
+                }
+            }
+        }
+        return s.size();
+    }
+};
