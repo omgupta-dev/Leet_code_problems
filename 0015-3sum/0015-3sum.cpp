660e1192dd4a404c80b08f3687717b50fa5cb6d6
Om Gupta <omgupta.react@gmail.com>
2026-09-26 16:47:55 +0530

    Time: 55 ms (26.4%), Space: 31 MB (19.91%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                    j++;
+                else if(sum>0)
+                    k--;
+                else
+                {
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j-1]==nums[j]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+        //TC=O(n^2)
+    }
+};
